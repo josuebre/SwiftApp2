c0c150ebf6427473739affa74c0e879890c0736b
Josue Breton Mini <josuebremini@josues-mac-mini.local>
2017-02-14 11:55:19 -0800

    Initial Commit

---
+++ Swift App 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App 2
+//
+//  Created by Josue Breton Mini on 2/14/17.
+//  Copyright © 2017 Josue Breton Mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
